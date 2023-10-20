4a92325905bb957935afdb429c61972bb54ac02f
YahyaNashar22 <cenations.people@live.com>
2023-10-20 16:58:55 +0300

    created MVC users and added hashing for passwords

--- index.js
+++ index.js
@@ -7,18 +7,30 @@ import hotelRoute from "./routes/hotel.js";
 import roomRoute from "./routes/room.js";
 import userRoute from "./routes/user.js";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const port = process.env.PORT || 8000;
 
 //middlewares
+app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
 app.use("/auth", authRoute);
 app.use("/hotel", hotelRoute);
 app.use("/room", roomRoute);
 app.use("/user", userRoute);
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
 
 const connect = async () => {
   try {
--- routes/room.js
+++ routes/room.js
@@ -1,5 +1,5 @@
 import express from "express";
-import Room from "../models/Room.js";
+// import Room from "../models/Room.js";
 
 const router = express.Router();
 
