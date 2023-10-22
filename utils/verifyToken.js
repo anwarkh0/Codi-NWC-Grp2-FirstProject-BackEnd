602549b0f7c4d5a772a6818c14f1d2117c7fb6f6
YahyaNashar22 <cenations.people@live.com>
2023-10-22 23:01:55 +0300

    finished users and auth

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -14,21 +14,21 @@ export const verifyToken = (req, res, next) => {
 };
 
 export const verifyUser = (req, res, next) => {
-  verifyToken(req, res, () => {
+  verifyToken(req, res, next, () => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
       next();
     } else {
-      if (err) return next(createErrpr(403, "You are not authorized"));
+      if (err) return next(createError(403, "You are not authorized"));
     }
   });
 };
 
 export const verifyAdmin = (req, res, next) => {
-  verifyToken(req, res, () => {
+  verifyToken(req, res, next, () => {
     if (req.user.isAdmin) {
       next();
     } else {
-      if (err) return next(createErrpr(403, "You are not authorized"));
+      if (err) return next(createError(403, "You are not authorized"));
     }
   });
 };
