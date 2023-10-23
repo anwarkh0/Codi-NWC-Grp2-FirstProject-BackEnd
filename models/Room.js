24b9403a81f6acbe6a266108ebbca98576e521bd
YahyaNashar22 <cenations.people@live.com>
2023-10-23 11:04:53 +0300

    MERGED

--- models/Room.js
+++ models/Room.js
@@ -1,46 +1,33 @@
-// import mongoose from "mongoose";
-// const { Schema } = mongoose;
-
-// const RoomSchema = new mongoose.Schema({});
-
-// export default mongoose.model("Room", RoomSchema);
-
-
-
-import mongoose, { Schema } from "mongoose"
+import mongoose, { Schema } from "mongoose";
 const RoomSchema = Schema({
-    name: {
-        type: String
-    },
-    price: {
-        type: Number
-    },
-    poeple: {
-        type: Number,
-        min: 1,
-        max: 2
-    },
-    servicesIcon: {
-        type: [String],
-    },
-    servicesDesc: {
-        type: [String]
-    },
-    rulesIcon: {
-        type: [String]
-    },
-    rulesDesc: {
-        type: [String]
-    },
-    dates: {
-        type: [String]
-    }
+  name: {
+    type: String,
+  },
+  price: {
+    type: Number,
+  },
+  poeple: {
+    type: Number,
+    min: 1,
+    max: 2,
+  },
+  servicesIcon: {
+    type: [String],
+  },
+  servicesDesc: {
+    type: [String],
+  },
+  rulesIcon: {
+    type: [String],
+  },
+  rulesDesc: {
+    type: [String],
+  },
+  dates: {
+    type: [String],
+  },
+});
 
-
-
-})
-
-
-const Room = new mongoose.model('Room', RoomSchema);
+const Room = new mongoose.model("Room", RoomSchema);
 
 export default Room;
