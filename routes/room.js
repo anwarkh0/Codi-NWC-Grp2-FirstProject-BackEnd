706cfd0eab43e3f5f2ba58cf9bb10cd1733a4422
jammal.souheir@gmail.com <jammal.souheir@gmail.com>
2023-10-23 09:59:06 +0300

    mvc for Room

--- routes/room.js
+++ routes/room.js
@@ -1,6 +1,28 @@
-import express from "express";
-import Room from "../models/Room.js";
+// import Room from "../models/Room.js";
 
-const router = express.Router();
+import { displayRooms, selectRoom, deleteRoom, editRoom, addRoom, displayRoomsByHotel } from '../controllers/room.js'
+
+
+import express from "express"
+const router = express.Router()
+
+
+//get all rooms available
+router.get('/', displayRooms)
+
+//get room by id (for dashboard and room services)
+router.get('/:id', selectRoom)
+
+//Remove Room(dashboard)
+router.delete('/:id', deleteRoom)  //with auth
+
+//edit Room(dashboard)
+router.patch('/:id', editRoom)    //with auth
+
+//add Room(dashboard)
+router.patch('/:id', addRoom)     //with auth
+
+//get rooms for a specific hotel
+router.get('/:id/rooms', displayRoomsByHotel)
 
 export default router;
