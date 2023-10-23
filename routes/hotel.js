e95a334e11885966624fb4027d7def77afb166b8
YahyaNashar22 <cenations.people@live.com>
2023-10-23 09:08:11 +0300

    finished Hotel mvc

--- routes/hotel.js
+++ routes/hotel.js
@@ -1,6 +1,18 @@
 import express from "express";
-// import Hotel from "../models/Hotel";
+import {
+  createHotel,
+  updateHotel,
+  deleteHotel,
+  getHotel,
+  getHotels,
+} from "../controllers/hotel.js";
 
 const router = express.Router();
+
+router.post("/", createHotel);
+router.put("/:id", updateHotel);
+router.delete("/:id", deleteHotel);
+router.get("/:id", getHotel);
+router.post("/", getHotels);
 
 export default router;
