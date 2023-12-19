5d6ee2cadc8f8c942688dcde2bad3be2f2d250e8
WouroudElKhaldi <wouroudelkhaldi@gmail.com>
2023-12-19 10:17:22 +0200

    switching connection to sequelize

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,19 +1,23 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
-
-export const connect = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO);
-    console.log("Connected to mongoDB");
-  } catch (error) {
-    throw error;
+import { Sequelize } from "sequelize";
+import "dotenv/config";
+// Your Sequelize configuration
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
   }
-};
+);
+// Test database connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database: ", error);
+  });
 
-mongoose.connection.on("disconnected", () => {
-  console.log("mongoDB disconnected");
-});
-mongoose.connection.on("connected", () => {
-  console.log("mongoDB connected");
-});
+export default sequelize;
--- index.js
+++ index.js
@@ -1,39 +1,43 @@
 import express from "express";
-import mongoose from "mongoose";
-const app = express();
+import sequelize from "./config/dbConnection.js";
 import dotenv from "dotenv";
 import roomRoute from "./routes/room.js";
 import userRoute from "./routes/user.js";
 import hotelRoute from "./routes/hotel.js";
 import bookingRoute from "./routes/booking.js";
-
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import { connect } from "./config/dbConnection.js";
-import { errorTemplate } from "./utils/error.js";
+
 dotenv.config();
 
-const port = process.env.PORT || 8000;
+const app = express();
+const port = process.env.PORT ;
 const staticDirectory = "./images";
 app.use("/images", express.static(staticDirectory));
 
 //middlewares
 app.use(cookieParser());
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+  optionsSuccessStatus: 200, 
+};
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use("/room", roomRoute);
 app.use("/user", userRoute);
 app.use("/hotel", hotelRoute);
 app.use("/booking", bookingRoute);
 
-mongoose.connection.on("disconnected", () => {
-  console.log("mongoDB disconnected");
-});
-mongoose.connection.on("connected", () => {
-  console.log("mongoDB connected");
-});
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Database synchronized successfully");
+  })
+  .catch((error) => {
+    console.error("Failed to synchronize database: ", error);
+  });
 
 app.listen(port, () => {
-  connect();
   console.log(`server is listening on port ${port}`);
 });
