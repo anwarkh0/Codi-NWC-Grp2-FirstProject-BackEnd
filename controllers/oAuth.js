524acfc567de7464f9966201f76a33f0ea77ec22
WouroudElKhaldi <wouroudelkhaldi@gmail.com>
2023-12-27 09:04:50 +0200

    fixing oAuth

--- controllers/oAuth.js
+++ controllers/oAuth.js
@@ -4,23 +4,30 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 export const google = async (req, res, next) => {
+    const {name , email , photo} = req.body
     try {
-        const user = await UsersModel.findOne({ where: { email: req.body.email } });
+        const user = await UsersModel.findOne({ where: { email } });
 
         if (user) {
             const token = jwt.sign({ id: user.id }, 'secretKey');
             const { password, ...rest } = user.get();
-            res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
+            res.cookie('token', token, { httpOnly: true }).status(200).json(rest);
         } else {
             const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
             const hashedPassword = await bcrypt.hash(generatedPassword, 10);
-            const generatedName = req.body.name.split(' ').join('').toLowerCase() + Math.random().toString(36).slice(-4);
+
+            // Assuming the Google data includes both first and last names
+            const fullName = name.split(' ');
+            const generatedFirstName = fullName[0] || 'DefaultFirstName';
+            const generatedLastName = fullName.length > 1 ? fullName.slice(1).join(' ') : 'DefaultLastName';
 
             const newUser = await UsersModel.create({
-                firstName: generatedName,
-                email: req.body.email,
+                firstName: generatedFirstName,
+                lastName: generatedLastName,
+                email: email,
                 password: hashedPassword,
-                image: req.body.photo,
+                image: photo,
+                role : 'Customer'
             });
 
             const token = jwt.sign({ id: newUser.id }, 'secretKey');
--- routes/oAuth.js
+++ routes/oAuth.js
@@ -1,8 +1,9 @@
+import { loggedInUser } from "../controllers/AuthController.js";
 import { google } from "../controllers/oAuth.js";
 import express from 'express'
 
 const googleRouter = express.Router()
 
-googleRouter.post('/' , google)
+googleRouter.post('/' , google , loggedInUser)
 
 export default googleRouter
